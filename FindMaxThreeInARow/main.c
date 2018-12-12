9fbe192a4de15a1637cd2267580d26e981fd8ca0
NiklasJang <niklasjang@gmail.com>
2018-12-12 20:37:26 +0900

    Before Remove Comment

--- FindMaxThreeInARow/main.c
+++ FindMaxThreeInARow/main.c
@@ -39,96 +39,74 @@ int getAbsolute(int num) {
 	}
 }
 
-int DFS(Node** map,int N, int M, int a, int b,int _sum, int _count) {
-	int sum = _sum;         
-	int count = _count; // Root Node 외에 몇 개의 value가 더해졌는지 나타냄
-	int sub_max = 0;
-
-	
-	printf("DFS is called.\n");
-	printf("Root Node is <%d,%d>\n", a, b);
-	printf("_sum is %d\n", _sum);
-	printf("_count is %d\n", _count);
-	printf("\n");
-
-
-	//Root Node 방문
-	printf("map[%d][%d].visit ==1\n", a, b);
-	map[a][b].visited = 1;
-
+void printMap(Node** map, int N, int M) {
 	//전체노트 출력해보기
-	printf("sum is %d, count is %d, sum_max is %d\n", sum, count, sub_max);
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			printf("<%d, %d> ",map[i][j].value, map[i][j].visited);
+			printf("<%d, %d> ", map[i][j].value, map[i][j].visited);
 		}
 		printf("\n");
 	}
 	printf("\n");
 
+}
+
+int DFS(Node** map,int N, int M, int a, int b,int _sum, int _count) {
+	int sum = _sum;     // <a,b>를 기준으로 깊이우선탐색을 했을 때 가장 큰 연속된 세 수의 합       
+	int count = _count; // Root Node 외에 몇 개의 value가 더해졌는지 나타냄
+	int sub_max = 0;    // Root Node의 4방향 중 한 방향에서 찾은 최대값?????????
+
+
+	//Root Node 방문
+	map[a][b].visited = 1;
+
+	printMap(map, N, M);
 	
 
 	for (int i = -1; i <= 1; i++) {      // -1 <= i <= +1
 		for (int j = -1; j <= 1; j++) {  // -1 <= j <= +1
-			// Root의 상하좌우일 때
-			printf("Root <%d,%d> 's <i,j> is <%d,%d>\n",a,b, i, j);
-			if (getAbsolute(i) + getAbsolute(j) == 1) {
+			//printf("Root <%d,%d> 's <i,j> is <%d,%d>\n",a,b, i, j);
+
+			if (getAbsolute(i) + getAbsolute(j) == 1) {// Root의 상하좌우일 때
+				if (a + i < 0 || N - 1 < a + i || b + j < 0 || M - 1 < b + j) continue; //접근 불가능한 인덱스 처리
 				
-				//접근 불가능한 인덱스 처리
-				if (a + i < 0 || N - 1 < a + i || b + j < 0 || M - 1 < b + j) {
-					//printf("불가능한 인덱스! <%d,%d>\n",a+i, b+j);
-					continue;
-				}
 				//Root Node의 상하좌우 중 하나의 값이 아직 접근하지 않았다면
 				if (map[a + i][b + j].visited == 0) {
-					if (count == 0) {
-						int last_ele = 0;
+					printf("<%d,%d> -> <%d,%d> successed", a, b, a + i, b + j);
+					if (count == 0) {//하나의 값을 더한 후 그곳에서 DFS를 call 해야하면
+						map[a + i][b + j].visited = 1;
 						sum += map[a + i][b + j].value;
 						count += 1;
-						map[a + i][b + j].visited = 1;
-						//전체노트 출력해보기
-						printf("sum is %d, count is %d, sum_max is %d\n", sum, count, sub_max);
-						for (int i = 0; i < N; i++) {
-							for (int j = 0; j < M; j++) {
-								printf("<%d, %d> ", map[i][j].value, map[i][j].visited);
-							}
-							printf("\n");
-						}
-						printf("\n");
+						
 
-						printf("\ncall procedure Root = <%d, %d>\n",a+i, b+j);
-						last_ele = DFS(map, N, M, a+i, b + j, sum, count);
-						sum += last_ele;
+						printf("ncall procedure Root = <%d, %d>\n",a+i, b+j);
+						int sub_sub_max = 0; //마지막 깊이에서 return된 값
+						sub_sub_max = DFS(map, N, M, a+i, b + j, sum, count);
+						count -= 1; //상하좌우 중 한 방향으로 가치지기가 끝나면 count -1
+						sum -= map[a + i][b + j].value; //이동해서 가치지기가 다시 시작했던 값을 빼준다. 
+						printf("sub_sub_max is %d\n", sub_sub_max);
+						if (sub_max < sub_sub_max) {
+							sub_max = sub_sub_max;
+						}
+					}
+					else if (count == 1) {//하나의 값만 더 더하면 되면
+						map[a + i][b + j].visited = 1;
+						if (getAbsolute(a + i) + getAbsolute(b + j) == 2) {//시작 Root 노드에 대해서 대각선이면
+							map[a + i][b + j].visited = 0;
+						}
+						sum += map[a + i][b + j].value;
+						printf("세 개의 합은 %d\n", sum);
 						if (sub_max < sum) {
 							sub_max = sum;
 						}
-						sum -= last_ele;
-						count -= 1;
-						printf("last_ele is %d\n", last_ele);
-						//전체노트 출력해보기
-						printf("sum is %d, count is %d, sum_max is %d\n", sum, count, sub_max);
-						for (int i = 0; i < N; i++) {
-							for (int j = 0; j < M; j++) {
-								printf("<%d, %d> ", map[i][j].value, map[i][j].visited);
-							}
-							printf("\n");
+						if (i == 1 && j == 0) {//사방을 다 확인했으면 return
+							return sub_max;
 						}
-						printf("\n");
+						sum -= map[a + i][b + j].value;
 					}
-					else if (count == 1) {
-						map[a + i][b + j].visited = 1;
-						count += 1;
-						//전체노트 출력해보기
-						printf("sum is %d, count is %d, sum_max is %d\n", sum, count, sub_max);
-						for (int i = 0; i < N; i++) {
-							for (int j = 0; j < M; j++) {
-								printf("<%d, %d> ", map[i][j].value, map[i][j].visited);
-							}
-							printf("\n");
-						}
-						printf("Return %d\n", map[a + i][b + j].value);
-						return map[a + i][b + j].value;
-					}
+				}
+				else {
+					printf("<%d,%d> -> <%d,%d> failed",a,b, a + i, b + j);
 				}
 			}
 		}
@@ -141,7 +119,7 @@ int DFS(Node** map,int N, int M, int a, int b,int _sum, int _count) {
 			map[i][j].visited = 0;
 		}
 	}
-	printf("이 DFS에서 최대합은 %d", sub_max);
+	printf("이 DFS에서 최대합은 %d\n\n", sub_max);
 	return sub_max;
 }
 
@@ -171,19 +149,18 @@ int main(void) {
 	}
 
 	//map의 모든 노드들에 대해서 DFS 알고리즘을 실행
-	/*for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			printf("<%d,%d> 노드를 기준으로 DFS 실행!!\n",i,j);
+			printf("<%d,%d> 노드를 기준으로 DFS 실행!!\n", i, j);
 			int sum_max = DFS(map, N, M, i, j, map[i][j].value, 0);
 			if (max < sum_max) {
 				max = sum_max;
 			}
 		}
-	}*/
-	int sum_max = DFS(map, N, M, 0, 0, map[0][0].value, 0);
-	printf("@@@@@@@@@");
-	printf("max is %d", sum_max);
-	printf("@@@@@@@@@");
+	}
+	printf("@@@@@@@@@\n");
+	printf("max is %d\n", max);
+	printf("@@@@@@@@@\n");
 
 	//할당 해제
 	for (int i = 0; i < N; i++) {
